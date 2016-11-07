d7253acb3fc0fb64f7ece746823e6f50380bc1e6
Edmund Alexander Noble <eanoble@strombola.csclub.uwaterloo.ca>
2016-11-07 15:31:01 -0500

    Czechpoint

---
+++ src/examples/Examples.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+import Test.Framework
+
+test_nonEmpty = do assertEqual [1] (myReverse [1])
+                   assertEqual [3,2,1] (myReverse [1,2,3])
+
+test_empty = assertEqual ([] :: [Int]) (myReverse [])
+
+main = htfMain htf_thisModulesTests
