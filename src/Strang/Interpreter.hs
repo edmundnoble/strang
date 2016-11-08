4358c43935aa9068ebb71c26a30d3446a4f0aa52
Edmund Noble <edmundnoble@gmail.com>
2016-11-07 23:41:42 -0500

    Dem lenses

--- src/Strang/Interpreter.hs
+++ src/Strang/Interpreter.hs
@@ -18,8 +18,9 @@
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Strang.Interpreter (interpretProgram,InterpreterOptions,OutputOptions) where
+module Strang.Interpreter (interpretProgram,InterpreterOptions,OutputOptions,showLog,outputOptions) where
 
   import Data.Text (Text)
   import qualified Data.Text.IO as TIO
@@ -29,15 +30,18 @@ module Strang.Interpreter (interpretProgram,InterpreterOptions,OutputOptions) wh
   import Strang.Prelude(leftMap)
   import Control.Monad.Writer.Strict hiding (sequence)
   import Data.Default
+  import Control.Lens
 
   parseProgram :: Text -> Either String (InputMode, AnyCommand)
   parseProgram pgm = leftMap (\e -> "Parsing error: " ++ show e) (parse programParser "" pgm)
 
-  data InterpreterOptions = InterpreterOptions { outputOptions :: OutputOptions }
-  data OutputOptions = OutputOptions { showLog :: Bool }
+  data InterpreterOptions = InterpreterOptions { _outputOptions :: OutputOptions }
+  data OutputOptions = OutputOptions { _showLog :: Bool }
+  makeLenses ''InterpreterOptions
+  makeLenses ''OutputOptions
 
   instance Default OutputOptions where
-    def = OutputOptions { showLog = False }
+    def = OutputOptions { _showLog = False }
 
   instance Default InterpreterOptions where
     def = InterpreterOptions def
@@ -51,7 +55,7 @@ module Strang.Interpreter (interpretProgram,InterpreterOptions,OutputOptions) wh
       modeAndCmd <- parseProgram cmd
       let (mode, cmd) = modeAndCmd
       composedProgram <- runCommand <$> withProgramType cmd
-      pure $ mode >>= (sequence_ . fmap (showProgramOutput (outputOptions opts) . composedProgram))
+      pure $ mode >>= (sequence_ . fmap (showProgramOutput (_outputOptions opts) . composedProgram))
 
   showProgramOutput :: OutputOptions -> CommandResult Text -> IO ()
   showProgramOutput _ = TIO.putStrLn . fst . runWriter
